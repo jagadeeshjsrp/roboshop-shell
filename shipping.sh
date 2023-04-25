1f0f147427cba8b1771a57f764dd54df7a8a2830
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-25 17:42:17 +0530

    catalogue script

--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,31 @@
+echo -e "\e[36m>>>>>>>>install maven <<<<<\e[0m"
 yum install maven -y
+
+echo -e "\e[36m>>>>>>>>create app user <<<<<\e[0m"
 useradd roboshop
+
+rm -rf /app
 mkdir /app
+
+echo -e "\e[36m>>>>>>>>download app content <<<<<\e[0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+
+echo -e "\e[36m>>>>>>>>extract app content<<<<<\e[0m"
 cd /app
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "\e[36m>>>>>>>>download maven dependencies <<<<<\e[0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
+echo -e "\e[36m>>>>>>>>install mysql  <<<<<\e[0m"
+yum install mysql -y
+
+echo -e "\e[36m>>>>>>>>load schema<<<<<\e[0m"
+mysql -h mysql-dev.devjsr99.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+
+echo -e "\e[36m>>>>>>>>start shipping service <<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable shipping
-systemctl start shipping
-
-yum install mysql -y
-mysql -h mysql-dev.devjsr99.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 systemctl restart shipping
