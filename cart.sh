f308a78b74cc73f76a139801673ad8dce727185b
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-25 17:12:51 +0530

    catalogue script

--- cart.sh
+++ cart.sh
@@ -1,12 +1,28 @@
+echo -e "\e[36m>>>>>>>>configure nodeJS repos <<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[33m>>>>>>>>install nodeJS  <<<<<\e[0m"
 yum install nodejs -y
+
+echo -e "\e[34m>>>>>>>>add application user <<<<<\e[0m"
 useradd roboshop
+rm -rf /app
 mkdir /app
+
+echo -e "\e[31m>>>>>>>>download app contenet <<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+
+echo -e "\e[36m>>>>>>>>unzip app content <<<<<\e[0m"
 unzip /tmp/cart.zip
+
+echo -e "\e[36m>>>>>>>>install dependencies <<<<<\e[0m"
 npm install
-cp cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>copy cart service  <<<<<\e[0m"
+cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[36m>>>>>>>>start cart service <<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable cart
 systemctl start cart
