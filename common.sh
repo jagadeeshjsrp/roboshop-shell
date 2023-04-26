9288b35fd81a13b26a077be1ae561ac1f569fd20
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-26 20:53:13 +0530

    correction using function in our script

--- common.sh
+++ common.sh
@@ -17,22 +17,22 @@ func_nodejs() {
 
 
   echo -e "\e[31m>>>>>>>>download app contenet <<<<<\e[0m"
-  curl -o /tmp/$(component).zip https://roboshop-artifacts.s3.amazonaws.com/$(component).zip
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
   echo -e "\e[36m>>>>>>>>unzip app content <<<<<\e[0m"
-  unzip /tmp/$(component).zip
+  unzip /tmp/${component}.zip
 
   echo -e "\e[36m>>>>>>>>install dependencies <<<<<\e[0m"
   npm install
 
   echo -e "\e[36m>>>>>>>>copy cart service  <<<<<\e[0m"
-  cp ${script_path}/$(component).service /etc/systemd/system/$(component).service
+  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
   echo -e "\e[36m>>>>>>>>start cart service <<<<<\e[0m"
   systemctl daemon-reload
-  systemctl enable $(component)
-  systemctl restart $(component)
+  systemctl enable ${component}
+  systemctl restart ${component}
 
 
 }
