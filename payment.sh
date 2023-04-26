ecf49b83e0294481af3ecf69f31ef9115f7fae05
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-26 10:39:58 +0530

    configure payment script

--- payment.sh
+++ payment.sh
@@ -20,7 +20,7 @@ echo -e "\e[36m>>>>>>>>install dependencies<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>setup systemD service<<<<<\e[0m"
-cp /root/roboshop-shell/payment.service /etc/systemd/system/payment.service
+cp /home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[36m>>>>>>>>start payment service<<<<<\e[0m"
 systemctl daemon-reload
