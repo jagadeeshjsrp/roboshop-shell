7e823c52712423fad233455f142c8350aa077cd3
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-26 10:22:43 +0530

    configure cart script

--- cart.sh
+++ cart.sh
@@ -21,7 +21,7 @@ echo -e "\e[36m>>>>>>>>install dependencies <<<<<\e[0m"
 npm install
 
 echo -e "\e[36m>>>>>>>>copy cart service  <<<<<\e[0m"
-cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
+cp /home/centos/roboshop-shell/cart.service /etc/systemd/system/cart.service
 
 echo -e "\e[36m>>>>>>>>start cart service <<<<<\e[0m"
 systemctl daemon-reload
