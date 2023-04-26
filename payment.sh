94ea6bf9a7603b9c46f2dbf64ddba10d3503e1c1
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-26 20:06:37 +0530

    correction in payment.sh file while using sed cmd

--- payment.sh
+++ payment.sh
@@ -25,7 +25,7 @@ echo -e "\e[36m>>>>>>>>install dependencies<<<<\e[0m"
 pip3.6 install -r requirements.txt
 
 echo -e "\e[36m>>>>>>>>setup systemD service<<<<<\e[0m"
-sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}" ${script_path}/payment.service
+sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service
 cp ${script_path}/payment.service /etc/systemd/system/payment.service
 
 echo -e "\e[36m>>>>>>>>start payment service<<<<<\e[0m"
