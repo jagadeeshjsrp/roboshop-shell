4201e6eba6ed214e1844eeeee2dd274ee3f18140
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-28 21:38:52 +0530

    correction

--- common.sh
+++ common.sh
@@ -65,7 +65,7 @@ func_app_prereq() {
 
     func_print_head "extract app content"
     cd /app
-    unzip /tmp/${component}.zip
+    unzip /tmp/${component}.zip  &>>$log_file
     func_stat_check $?
 
 }
