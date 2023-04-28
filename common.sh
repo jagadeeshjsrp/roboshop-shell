6829196da0ed960e92455704860859c6f986bd4c
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-28 22:31:03 +0530

    correction

--- common.sh
+++ common.sh
@@ -60,7 +60,7 @@ func_app_prereq() {
     func_stat_check $?
 
     func_print_head "download application content"
-    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip &>>$log_file
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log_file
     func_stat_check $?
 
     func_print_head "extract app content"
