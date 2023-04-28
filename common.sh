fa975e1af3e42684fd51580f0a5f480f31d55360
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-28 11:49:03 +0530

    extend functions using special variables

--- common.sh
+++ common.sh
@@ -1,35 +1,40 @@
 app_user=roboshop
+script=$(realpath "$0")
+script_path=$(dirname "$script")
 
+print_head() {
+  echo -e "\e[35m>>>>>>>> $1 <<<<<<<<\e[0m"
+}
 
 
 func_nodejs() {
 
-  echo -e "\e[36m>>>>>>>>configure nodeJS repos <<<<<\e[0m"
+  print_head "configure nodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  echo -e "\e[33m>>>>>>>>install nodeJS  <<<<<\e[0m"
+  print_head "install nodeJS"
   yum install nodejs -y
 
-  echo -e "\e[34m>>>>>>>>add application user <<<<<\e[0m"
+  print_head "add application user"
   useradd ${app_user}
   rm -rf /app
   mkdir /app
 
 
-  echo -e "\e[31m>>>>>>>>download app contenet <<<<<\e[0m"
+  print_head "download app contenet"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
 
-  echo -e "\e[36m>>>>>>>>unzip app content <<<<<\e[0m"
+  print_head "unzip app content"
   unzip /tmp/${component}.zip
 
-  echo -e "\e[36m>>>>>>>>install dependencies <<<<<\e[0m"
+  print_head "install dependencies"
   npm install
 
-  echo -e "\e[36m>>>>>>>>copy cart service  <<<<<\e[0m"
+  print_head "copy cart service"
   cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-  echo -e "\e[36m>>>>>>>>start cart service <<<<<\e[0m"
+  print_head "start cart service"
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
