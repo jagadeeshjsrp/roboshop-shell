c0a94506ebe91e3bf5af966d7c2b20db14e8e6c9
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-28 17:05:02 +0530

    shipping changes

--- common.sh
+++ common.sh
@@ -2,56 +2,84 @@ app_user=roboshop
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 
-print_head() {
+func_print_head() {
   echo -e "\e[35m>>>>>>>> $1 <<<<<<<<\e[0m"
 }
 
-schema_setup() {
+func_schema_setup() {
 if [ "$schema_setup" == "mango" ]; then
-  print_head "copy mongodb repo"
+  func_print_head "copy mongodb repo"
   cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
 
- print_head "install mongodb client"
+  func_print_head "install mongodb client"
   yum install mongodb-org-shell -y
 
-  print_head "load schema"
+  func_print_head "load schema"
   mongo --host mongodb-dev.devjsr99.online </app/schema/${component}.js
+fi
+if  [ "${schema_setup}" == "mysql" ]; then
+
+  func_print_head "install mysql client"
+  yum install mysql -y
+
+  func_print_head "load schema"
+  mysql -h mysql-dev.devjsr99.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
+
 fi
 }
 
-func_nodejs() {
+func_app_prereq() {
+   func_print_head "create app user"
+    useradd ${app_user}
 
-  print_head "configure nodeJS repos"
+    func_print_head "create app Directory"
+    rm -rf /app
+    mkdir /app
+
+    func_print_head "download application content"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip
+
+    func_print_head "extract app content"
+    cd /app
+    unzip /tmp/${component}.zip
+
+}
+
+func_systemd_setup() {
+   func_print_head "setup systemD service"
+    cp ${script_path}/${component}.service /etc/systemd/system/{component}.service
+
+    func_print_head "start shipping service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl restart ${component}
+}
+func_nodejs() {
+  func_print_head "configure nodeJS repos"
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-  print_head "install nodeJS"
+  func_print_head "install nodeJS"
   yum install nodejs -y
 
-  print_head "add application user"
-  useradd ${app_user}
-  rm -rf /app
-  mkdir /app
+  func_app_prereq
 
-
-  print_head "download app contenet"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-  cd /app
-
-  print_head "unzip app content"
-  unzip /tmp/${component}.zip
-
-  print_head "install dependencies"
+  func_print_head "install nodeJS dependencies"
   npm install
 
-  print_head "copy cart service"
-  cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
+  func_schema_setup
+  func_systemd_setup
+}
+func_java() {
+  func_print_head "install maven"
+  yum install maven -y
 
-  print_head "start cart service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+  func_app_prereq
 
-  schema_setup
+  func_print_head "download maven dependencies"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
 
+  func_schema_setup
+  func_systemd_setup
 }
--- shipping.sh
+++ shipping.sh
@@ -8,38 +8,7 @@ if [ -z "$mysql_root_password" ]; then
   exit
 fi
 
-echo -e "\e[36m>>>>>>>>install maven <<<<<\e[0m"
-yum install maven -y
 
-echo -e "\e[36m>>>>>>>>create app user <<<<<\e[0m"
-useradd ${app_user}
-
-rm -rf /app
-mkdir /app
-
-echo -e "\e[36m>>>>>>>>download app content <<<<<\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-
-echo -e "\e[36m>>>>>>>>extract app content<<<<<\e[0m"
-cd /app
-unzip /tmp/shipping.zip
-
-echo -e "\e[36m>>>>>>>>download maven dependencies <<<<<\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "\e[36m>>>>>>>>install mysql  <<<<<\e[0m"
-yum install mysql -y
-
-echo -e "\e[36m>>>>>>>>load schema<<<<<\e[0m"
-mysql -h mysql-dev.devjsr99.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-
-echo -e "\e[36m>>>>>>>>setup systemD service<<<<<\e[0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-
-
-echo -e "\e[36m>>>>>>>>start shipping service <<<<<\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+component="shipping"
+schema_setup=mysql
+func_java()
