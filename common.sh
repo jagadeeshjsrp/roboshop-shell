476b0cb7f5045e8681ee41e22924a947b1d7f85c
jagadeesh p <jagadeeshpc5@gmail.com>
2023-04-29 07:41:49 +0530

    payment setup

--- common.sh
+++ common.sh
@@ -112,3 +112,22 @@ func_java() {
   func_schema_setup
   func_systemd_setup
 }
+
+func_python() {
+  func_print_head "install python"
+  yum install python36 gcc python3-devel -y &>>$log_file
+  func_stat_check $?
+
+  func_app_prereq
+
+  func_print_head "install dependencies"
+  pip3.6 install -r requirements.txt &>>$log_file
+  func_stat_check $?
+
+  func_print_head "update passwords in system service file"
+  sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service &>>$log_file
+  func_stat_check $?
+
+  func_schema_setup
+
+}
